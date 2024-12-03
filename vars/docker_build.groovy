d2c9007af689eca21836880c9631b77bd472ec9b
Sayantan Manna <sayantanmanna06@gmail.com>
2024-12-04 01:54:40 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
-def call(String ProjectName, String ImageTag, String DockerHubUser){
+def call(ProjectName, ImageTag, DockerHubUser){
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
